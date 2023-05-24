78691a967a9d3908a2cdde45834c03f1ce4fdcc3
Yasser-Slaymen <yaserslyman0@gmail.com>
2023-05-24 16:03:04 +0200

    design dubelchecking

--- src/libhome/atomic/FlexibleBtn.tsx
+++ src/libhome/atomic/FlexibleBtn.tsx
@@ -1,19 +1,11 @@
-import styles from '../../HomeStyles/atomic/GetStartedBtn.module.css'
-import { CounterDisplay } from "../../Storage/ConnterDisplay";
-
-   
-
+import styles from "../../HomeStyles/atomic/GetStartedBtn.module.css";
 export default function FlexibleBtn() {
-    const value = CounterDisplay();
-  
-    const elementorURL = `https://www.liveryvideo.com/pricing/?sliderValue=${value}`;
-    return(
-        <a className={styles.GetStartedBtn} href={elementorURL} target="_blank" rel="noopener noreferrer">see pricing</a>
-    )
+  return (
+    <a
+      className={styles.GetStartedBtn}
+      href="https://www.liveryvideo.com/pricing/"
+    >
+      see your pricing
+    </a>
+  );
 }
-// import styles from '../../HomeStyles/atomic/GetStartedBtn.module.css'
-// export default function FlexibleBtn() {
-//     return(
-//         <a className={styles.GetStartedBtn} href="https://www.liveryvideo.com/pricing/">see pricing</a>
-//     )
-// }
